097aa361cf4e26a9e035144670002d8b9a6f2337
Greg chen <greg.chen@suryani.cn>
2014-10-20 17:36:44 +0800

    v1

---
+++ src/main/java/com/ujoku/domain/Member.java
@@ -0,0 +1,17 @@
+package com.ujoku.domain;
+
+import java.util.Date;
+
+/**
+ * Created by greg.chen on 14-10-20.
+ */
+public class Member {
+    private int user_id;
+    private String user_name;
+    private String email;
+    private String password;
+    private String real_name;
+    private Short gender;
+    private Date birthday;
+
+}
